077ec7df58e7f9e3642c3af0d4ab212d9780a707
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-09-23 16:09:00 -0300

    Update l3q21.c

--- lista3resolvida/l3q21.c
+++ lista3resolvida/l3q21.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*21)	Escreva as seguintes sequências de números: (1, 1 2 3 4 5 6 7 8 9 10) (2, 1 2 3 4 5 6 7 8 9 10)
-/(3, 1 2 3 4 5 6 7 8 9 10) (4, 1 2 3 4 5 6 7 8 9 10) e assim sucessivamente, até que o
-/primeiro número (antes da vírgula), também chegue a 10.
+/*21)	Escreva as seguintes sequÃªncias de nÃºmeros: (1, 1 2 3 4 5 6 7 8 9 10) (2, 1 2 3 4 5 6 7 8 9 10)
+/(3, 1 2 3 4 5 6 7 8 9 10) (4, 1 2 3 4 5 6 7 8 9 10) e assim sucessivamente, atÃ© que o
+/primeiro nÃºmero (antes da vÃ­rgula), tambÃ©m chegue a 10.
 */
 
 int main()
 {
-    int x=1;
+    int x;
 
-    while(x<=10)
+    for(x=1;x<=10;x++)
     {
-        printf("%i, 1 2 3 4 5 6 7 8 9 10\n",x);
-        x++;
+        printf("(%i, 1 2 3 4 5 6 7 8 9 10)\n",x);
     }
     return 0;
 }

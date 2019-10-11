22c4ce7c74f25f7448f3791c1b4a34ad0478d529
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-10-11 13:49:10 -0300

    Create l6q7.c

---
+++ lista6resolvida/l6q7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+
+int x,y,z,p,s;
+char n1[30],n2[30];
+
+printf("Insira a primeira string: ");
+gets(n1);
+printf("Insira a segunda string: ");
+gets(n2);
+y=0;
+z=0;
+for(x=0;n1[x]!='\0';x++){
+        p=("%d",n1[x]);
+        y+=p;
+        }
+for(x=0;n2[x]!='\0';x++){
+        s=("%d",n2[x]);
+        z+=s;
+        }
+if(z==y) printf("As strings tem o mesmo valor: %d.\n",y);
+else if(y>z) printf("O valor da primeira string(%d) e maior que o valor da segunda string(%d).\n",y,z);
+else if(y<z) printf("O valor da segunda string(%d) e maior que o valor da primeira string(%d).\n",z,y);
+}

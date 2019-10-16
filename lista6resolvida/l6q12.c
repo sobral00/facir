eb60cf01d01ccf3e49eced25812e01b5ac17100e
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-10-16 11:37:09 -0300

    Create l6q12.c

---
+++ lista6resolvida/l6q12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+int x, y=0;
+char s[30], c;
+
+printf("Insira uma string: ");
+gets(s);
+printf("Insira um caractere: ");
+scanf("%c", &c);
+
+for(x=0; s[x]!='\0'; x++)
+    {
+    if(s[x]=='a' || s[x]=='e' || s[x]=='i' || s[x]=='o' || s[x]=='u')
+        {
+    s[x]=c;
+    y++;
+        }
+    }
+printf("A string tem %d vogais.\n", y);
+printf("%s", s);
+}

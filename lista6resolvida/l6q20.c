b69110c1d34e26e2db2bf828c0e3695fd1942619
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-10-16 14:26:19 -0300

    Create l6q20.c

---
+++ lista6resolvida/l6q20.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main(){
+char nome1[20],nome2[20];
+int x,y,z;
+
+printf("insira uma string: ");
+gets(nome1);
+printf("insira outra string: ");
+gets(nome2);
+x=strlen(nome1);
+z=strlen(nome2);
+printf("Primeira String invertida e com os A's trocados por *: ");
+
+for(y=x-1;y>=0;y--){
+        if(nome1[y]=='A'){
+            nome1[y]='*';
+            printf("%c",nome1[y]);
+        }else{
+            printf("%c",nome1[y]);
+        }
+    }
+printf("\nSegunda String invertida e com os A's trocados por *: ");
+for(y=z-1;y>=0;y--){
+        if(nome2[y]=='A'){
+            nome2[y]='*';
+            printf("%c",nome2[y]);
+        }else{
+            printf("%c",nome2[y]);
+        }
+    }
+}

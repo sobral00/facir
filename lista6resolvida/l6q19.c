ff06488810e793ed36592de9cdc284c6ebd05502
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-10-16 14:16:03 -0300

    Create l6q19.c

---
+++ lista6resolvida/l6q19.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void main(){
+typedef struct lista{
+
+char nome[15];
+int idade;
+
+}Tlista;
+
+Tlista pessoa[10];
+int media,y=0,z=0;
+
+    for(int x=0;x<9;x++){
+    printf("Insira seu primeiro nome: ");
+    scanf("%s",&pessoa[x].nome);
+    setbuf(stdin,NULL);
+    printf("Insira sua idade: ");
+    scanf("%d",&pessoa[x].idade);
+    setbuf(stdin,NULL);
+        if(pessoa[x].idade < 0 ) break; else if(media>pessoa[x].idade) y+=pessoa[x].idade; z+=1;
+    }
+
+media=y/z;
+
+printf("\n- Pessoas Mais Velhas -\n");
+for(int x=0;x<z;x++){
+    if(media<pessoa[x].idade) {
+        printf("Nome: %s, Idade: %d\n",pessoa[x].nome,pessoa[x].idade);
+        }
+    }
+printf("\n- Pessoas Mais Novas -\n");
+for(int x=0;x<z;x++){
+    if(media>=pessoa[x].idade) {
+        if(pessoa[x].idade > 0) printf("Nome: %s, Idade: %.d\n",pessoa[x].nome,pessoa[x].idade);
+        }
+    }
+}
+
